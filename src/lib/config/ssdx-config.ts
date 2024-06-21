a52bf7a1e900dcc8ad5e0653111857f08d1971ff
John Føreland <john.foreland@me.com>
2024-06-21 14:16:21 +0200

    renamed ssdx.config.json → ssdx-config.json

--- src/lib/config/ssdx-config.ts
+++ src/lib/config/ssdx-config.ts
@@ -2,5 +2,5 @@ import fs from 'fs';
 import ssdxConfig from '../../dto/ssdx-config.dto.js';
 
 export function fetchConfig(): ssdxConfig {
-  return JSON.parse(fs.readFileSync('ssdx.config.json', 'utf8')) as ssdxConfig;
+  return JSON.parse(fs.readFileSync('ssdx-config.json', 'utf8')) as ssdxConfig;
 }
