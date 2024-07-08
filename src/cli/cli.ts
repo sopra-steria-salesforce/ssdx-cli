7746b9b9cb79286328bffbb73e35237e49c04c45
John Føreland <john.foreland@me.com>
2024-07-08 11:57:13 +0200

    log versions

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -2,6 +2,7 @@ import { Command } from 'commander';
 import CreateCommand from './create/create.command.js';
 import AuthCommand from './auth/auth.command.js';
 import { ResourceCommand } from './resource-assignment-manager/resource.command.js';
+import { logger } from 'lib/log.js';
 
 export default class cli {
   protected static program = new Command();
@@ -13,6 +14,8 @@ export default class cli {
   }
 
   public run(): void {
+    logger.info(`SSDX version: ${process.env.npm_package_version}`);
+    logger.info(`Node version: ${process.version}`);
     cli.program
       .name('ssdx-cli')
       .description('Salesforce DX cli helper tool')
