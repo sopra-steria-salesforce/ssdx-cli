ad78c14d3a1ab6ff947125030fedb311b127259c
John Arne Føreland <john.foreland@me.com>
2026-01-21 16:24:50 +0100

    removed unused method

--- src/lib/command-helper.ts
+++ src/lib/command-helper.ts
@@ -41,21 +41,6 @@ export interface CmdResult {
   code: number;
 }
 
-// TODO: move to new method
-
-export async function runCmd(cmd: string, args: string[] = []): Promise<string> {
-  const output = await spawn(cmd, args, {
-    shell: true,
-    encoding: 'utf8',
-  }).catch(error => {
-    print.error('Error running command:');
-    print.code(`${cmd} ${args.join(' ')}`);
-    throw error;
-  });
-
-  return output.stdout as string;
-}
-
 /* -------------------------------------------------------------------------- */
 /*                                  Cmd Class                                 */
 /* -------------------------------------------------------------------------- */
